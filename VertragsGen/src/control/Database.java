646d8f3b93f901cac429234b077cfe7205d5c889
sewerynkozlowski <seko1702@gmail.com>
2018-06-06 13:00:30 +0200

    WriteKfz Komplett ArrayList Write für alle Delete

--- VertragsGen/src/control/Database.java
+++ VertragsGen/src/control/Database.java
@@ -4,11 +4,13 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import model.JuristischePartei;
 import model.NatuerlichePartei;
-
+import model.Vertrag;
 import model.KfzKaufvertrag;
+import model.Mietvertrag;
 
 public class Database {
 	
@@ -81,6 +83,77 @@ public class Database {
 		}
 	}
 	
+	
+	public static ArrayList<String> readArrayListCols(int id, String tabellenName){
+		
+		conn = DatabaseCon.connect();
+		Statement sta;
+		try {
+			sta = conn.createStatement();
+			String queryUnfall = "select * from "+tabellenName+" where KfzKaufvertrag_ID="+id;
+			ResultSet rsUnfall = sta.executeQuery(queryUnfall);
+			ArrayList<String> unfallschaeden = new ArrayList<>();
+			
+			while(rsUnfall.next()) {
+			
+				unfallschaeden.add(rsUnfall.getString("bezeichnung"));
+				System.out.println(rsUnfall.getString("bezeichnung"));
+		
+			}
+			return unfallschaeden;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	
+		
+	}
+	
+	
+	public static KfzKaufvertrag readKfzKaufvertrag(int id) {
+		
+		
+		try {
+			
+			conn = DatabaseCon.connect();
+			Statement sta = conn.createStatement();
+			String query = "select * from KfzKaufvertrag where KfzKaufvertrag_ID="+ id;
+			ResultSet rsVertrag = sta.executeQuery(query);
+			
+			KfzKaufvertrag vertrag = new KfzKaufvertrag();
+			
+			vertrag.setId(id);
+			vertrag.setAlleinigesEigentum(intConv(rsVertrag.getInt("alleinigesEigentum")));
+			vertrag.setAustauschmotor(intConv(rsVertrag.getInt("austauschmotor")));
+			vertrag.setAustauschmotorLaufleistung(rsVertrag.getInt("austauschmotorLaufleistung"));
+			vertrag.setUnfallschaden(intConv(rsVertrag.getInt("unfallschaden")));
+			vertrag.setUmmeldungUnverzueglich(intConv(rsVertrag.getInt("ummeldungUnverzueglich")));
+			vertrag.setFahrzeugAbgemeldet(intConv(rsVertrag.getInt("fahrzeugAbgemeldet")));
+			vertrag.setFahrzeugschein(intConv(rsVertrag.getInt("fahrzeugschein")));
+			vertrag.setFahrzeugbrief(intConv(rsVertrag.getInt("fahrzeugbrief")));
+			vertrag.setStillegungsBescheinigung(intConv(rsVertrag.getInt("stillegungsBescheinigung")));
+			vertrag.setUntersuchungsbericht(intConv(rsVertrag.getInt("untersuchungsbericht")));
+			vertrag.setAnzahlSchluessel(rsVertrag.getInt("anzahlSchluessel"));
+			vertrag.setKaufpreis(rsVertrag.getInt("kaufpreis"));
+			vertrag.setAnzahlung(rsVertrag.getInt("anzahlung"));
+					
+			vertrag.setListeUnfallschaeden(readArrayListCols(id, "unfallschaeden"));
+			vertrag.setBeschaedigungen(readArrayListCols(id, "beschaedigungen"));
+			vertrag.setSondervereinbarungen(readArrayListCols(id, "sondervereinbarungen"));
+			
+			return vertrag;
+		
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		
+		
+	}
+	
 	//Speichert eine Juristische Partei in die Datenbank 
 public static void writeJuristischePartei(JuristischePartei jp) {
 		
@@ -139,6 +212,17 @@ public static void writeJuristischePartei(JuristischePartei jp) {
 			 return null;}
 	}
 
+	public static void writeKfzKaufvertragKomplett(KfzKaufvertrag vertrag) {
+		
+		
+		writeKfzKaufvertrag(vertrag);
+		
+		writeArrayListCols("Unfallschaeden", vertrag.getListeUnfallschaeden(), "KfzKaufvertrag");
+		writeArrayListCols("Sondervereinbarungen", vertrag.getSondervereinbarungen(), "KfzKaufvertrag");
+		writeArrayListCols("Beschaedigungen", vertrag.getBeschaedigungen(), "KfzKaufvertrag");
+		
+	}
+	
 	public static void writeKfzKaufvertrag(KfzKaufvertrag vertrag) {
 		
 		try {
@@ -172,8 +256,16 @@ public static void writeJuristischePartei(JuristischePartei jp) {
     	Connection conn = DatabaseCon.connect();
     	try {
 			Statement sta = conn.createStatement();
-			String sql2 = "insert into Unfallschaeden values (null, "
-					+ "(SELECT seq FROM sqlite_sequence WHERE name = KfzKaufvertrag),"+vertrag.getBezeichnung()+")";
+			
+			ResultSet rs = sta.executeQuery("SELECT seq FROM sqlite_sequence WHERE name = 'KfzKaufvertrag'");
+			int id = rs.getInt(1);
+			
+			System.out.println(id);
+			
+			String sql2 = "insert into Unfallschaeden values (null, "+id+" , '"+vertrag.getBezeichnung()+"')";
+//			SELECT seq FROM sqlite_sequence WHERE name = KfzKaufvertrag
+			
+			System.out.println(sql2);
 			sta.executeUpdate(sql2);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -181,6 +273,60 @@ public static void writeJuristischePartei(JuristischePartei jp) {
 			 
 		}
 	}
+    
+    public static void writeArrayListCols(Vertrag vertrag, String tabellenName, ArrayList<String> liste) {
+    	
+    	Connection conn = DatabaseCon.connect();
+    	try {
+			Statement sta = conn.createStatement();
+			
+			ResultSet rs = sta.executeQuery("SELECT seq FROM sqlite_sequence WHERE name = 'KfzKaufvertrag'");
+			int id = rs.getInt(1);
+			
+			
+			
+			for (String schaden : liste) {
+				
+				String sql2 = "insert into "+tabellenName+" values (null, "+id+" , '"+schaden+"')";
+				sta.executeUpdate(sql2);
+				
+			}
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    }
+    
+ public static void writeArrayListCols(String tabellenName, ArrayList<String> liste, String vertragsArt) {
+    	
+    	Connection conn = DatabaseCon.connect();
+    	try {
+			Statement sta = conn.createStatement();
+			
+			ResultSet rs = sta.executeQuery("SELECT seq FROM sqlite_sequence WHERE name = '"+vertragsArt+"'");
+			int id = rs.getInt(1);
+			
+			
+			
+			for (String schaden : liste) {
+				
+				String sql2 = "insert into "+tabellenName+" values (null, "+id+" , '"+schaden+"')";
+				sta.executeUpdate(sql2);
+				
+			}
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    }
+    
+    
 	
 	//Loescht eine Partei aus der Datenbank
 	public static void deletePartei(int id) {
@@ -203,6 +349,25 @@ public static void writeJuristischePartei(JuristischePartei jp) {
     	
     	
     }
+	
+	public static void deleteTable(int id, String tabellenName) {
+		
+		try {
+			conn = DatabaseCon.connect();
+			Statement sta = conn.createStatement();
+			
+			String query = "delete from"+tabellenName+" where "+tabellenName+"_ID="+ id;
+			sta.executeUpdate(query);
+			
+			System.out.println(tabellenName+" deleted");
+			conn.close();
+			
+		}
+		catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		
+	}
 
 	public static int booleanConv(boolean bool) {
 
@@ -212,25 +377,14 @@ public static void writeJuristischePartei(JuristischePartei jp) {
 			return 0;
 
 	}
+	
+	public static boolean intConv(int i) {
+		if(i == 0)
+			return false;
+		else
+			return true;
+	}
 
-	// public KfzKaufvertrag readKfzKaufvertrag(int ID) {
-	//
-	// Statement sta;
-	// try {
-	// Connection conn = DatabaseCon.connect();
-	// sta = conn.createStatement();
-	// String sql = "SELECT * FROM KfzKaufvertrag WHERE id ="+ID+"";
-	// ResultSet rs = sta.executeQuery(sql);
-	// rs.next();
-	// } catch (SQLException e1) {
-	// // TODO Auto-generated catch block
-	// e1.printStackTrace();
-	// }
-	//
-	//
-	//
-	//
-	//
-	// }
+
 
 }
